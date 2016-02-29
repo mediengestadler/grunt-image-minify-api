2eb1063d29b6593657d47f18e65e467e1e70a7f9
Yannic Stadler <ys@redshot.biz>
2016-02-29 11:43:28 +0100

    Update minifier.js

--- tasks/minifier.js
+++ tasks/minifier.js
@@ -41,51 +41,76 @@ module.exports = function(grunt) {
                 });
 
                 grunt.log.writeln(file + ': ' + message + ' - Original image was copied');
+            },
+            unixifyPath = function(filepath) {
+                if (process.platform === 'win32') {
+                    return filepath.replace(/\\/g, '/');
+                } else {
+                    return filepath;
+                }
+            },
+            detectDestType = function(dest) {
+                if (grunt.util._.endsWith(dest, '/')) {
+                    return 'directory';
+                } else {
+                    return 'file';
+                }
             };
 
-        this.files.forEach(function (file) {
-            var sendfile = cwd + file.src;
 
-            // api
-            request.post({
-                url: options.api_host + '/minify',
-                formData: {
-                    api_key: options.api_key,
-                    image: fs.createReadStream(sendfile)
-                }
-            }, function (err, httpResponse, res) {
-                // conv json
-                if (typeof(res) === 'string') {
-                    res = JSON.parse(res);
+        this.files.forEach(function (filePair) {
+            var isExpandedPair = filePair.orig.expand || false;
+
+            filePair.src.forEach(function(file) {
+                file = unixifyPath(file);
+                var dest = unixifyPath(filePair.dest);
+
+                if (detectDestType(dest) === 'directory') {
+                  dest = isExpandedPair ? dest : path.join(dest, file);
                 }
 
-                // fail
-                if (res.success && res.success === false) {
-                    if (res.code === 403) {
-                        // api key not valid
-                        return grunt.log.writeln(res.message);
-                    } else if (res.code === 404) {
-                        // action not found
-                        return grunt.log.writeln(res.message);
-                    } else if (res.code === 500) {
-                        //
-                        onError(file, res.message);
-                    } else {
-                        // everything else
-                        onError(file, res.message);
+                // api
+                request.post({
+                    url: options.api_host + '/minify',
+                    formData: {
+                        api_key: options.api_key,
+                        image: fs.createReadStream(file)
+                    }
+                }, function (err, httpResponse, res) {
+
+                    // conv json
+                    if (typeof(res) === 'string') {
+                        res = JSON.parse(res);
                     }
 
-                    return;
-                }
+                    // fail
+                    if (res.success && res.success === false) {
+                        if (res.code === 403) {
+                            // api key not valid
+                            return grunt.log.writeln(res.message);
+                        } else if (res.code === 404) {
+                            // action not found
+                            return grunt.log.writeln(res.message);
+                        } else if (res.code === 500) {
+                            //
+                            onError(file, res.message);
+                        } else {
+                            // everything else
+                            onError(file, res.message);
+                        }
 
-                // success
-                var image = new Buffer(res.image, 'base64');
+                        return;
+                    }
 
-                grunt.file.mkdir(path.dirname(file.dest));
+                    // success
+                    var image = new Buffer(res.image, 'base64');
 
-                fs.writeFile(file.dest, image, function () {
-                    grunt.log.writeln(file.src + ': ' + chalk.cyan('saved ' + res.saving + '%'));
-                    isDone(file);
+                    grunt.file.mkdir(path.dirname(dest));
+
+                    fs.writeFile(dest, image, function () {
+                        grunt.log.writeln(file + ': ' + chalk.cyan('saved ' + res.saving + '%'));
+                        isDone(file);
+                    });
                 });
             });
         });
